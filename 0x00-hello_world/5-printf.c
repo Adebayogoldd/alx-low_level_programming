99cc3adfd39865e5325a4c80079ad2c9e7a956aa
Adebayogoldd <yusufabdulgafar54@gmail.com>
2022-07-01 01:46:54 -0700

    lost

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
-#include <unistd.h>
+#include <stdio.h> 
 /**
-* main - prints a standard error
+* main - Entry point
 *
-* Return: Always 1 (Success)
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
-}
-fprintf(stderr, "and that piece of art is useful\" - \
-Dora Korpar, 2015-10-19\n");
-return (1);
+printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0);
 }
